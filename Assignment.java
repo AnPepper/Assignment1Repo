e4e5fc018ff803f10a85882a2944560a47911c4c
AnPepper <mxq19@txstate.edu>
2026-09-11 20:20:03 -0500

    printInventory method

---
+++ Assignment.java
@@ -0,0 +1,8 @@
+public class Assignment {
+    public static void main(String[] args) {
+        //Parallel Arrays
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+    }
+}
