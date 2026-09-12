0b1bababb492f98d672773f4301ec35f94ff53e5
AnPepper <mxq19@txstate.edu>
2026-09-11 20:36:48 -0500

    printInv println change

--- Assignment.java
+++ Assignment.java
@@ -5,4 +5,11 @@ public class Assignment {
         double[] itemPrices = new double[10];
         int[] itemStocks = new int[10];
     }
+  
+    public static void printInventory(String[] names, double[] prices, int[] stocks){
+        for (int i = 0; i < names.length; i++){
+            if (names[i] != null)
+                System.out.println("Item: " + names[i] + " | Price: " + prices[i]  + " | Stock: " + stocks[i]);
+        }
+    }
 }
