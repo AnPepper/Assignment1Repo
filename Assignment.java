fcfaf53bb54d1a5dfe62dbe2abc43d4ffc51c6f9
AnPepper <mxq19@txstate.edu>
2026-09-12 19:22:46 -0500

    restockItem method

--- Assignment.java
+++ Assignment.java
@@ -9,7 +9,17 @@ public class Assignment {
     public static void printInventory(String[] names, double[] prices, int[] stocks){
         for (int i = 0; i < names.length; i++){
             if (names[i] != null)
-                System.out.println("Item: " + names[i] + " | Price: " + prices[i]  + " | Stock: " + stocks[i]);
+                System.out.println("Item: " + names[i] + " | Price: " + prices[i]  + " | In Stock: " + stocks[i]);
         }
+    }
+
+    public static void restockItem(String[] names, int[] stocks, String target, int amount){
+        for (int i=0; i < names.length; i++){
+            if(names[i].equalsIgnoreCase(target)){
+                stocks[i] += amount;
+                break;
+            }
+        }
+
     }
 }
