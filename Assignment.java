81786e24766880ddb1cb77747a3021eb8a2c923a
AnPepper <mxq19@txstate.edu>
2026-09-13 20:44:13 -0500

    Menu feature added

--- Assignment.java
+++ Assignment.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Assignment {
     public static void main(String[] args) {
         //Parallel Arrays
@@ -5,12 +7,49 @@ public class Assignment {
         double[] itemPrices = new double[10];
         int[] itemStocks = new int[10];
 
-        //test data
-        itemNames[8] = "Banaa";
-        itemPrices[8] = 0.59;
-        itemStocks[8] = 30;
-         restockItem(itemNames, itemStocks, "Banana", 20);
-        printInventory(itemNames, itemPrices, itemStocks);
+        //test 
+        itemNames[9] = "Banana";
+        itemPrices[9] = 0.59;
+        itemStocks[9] = 30;
+        itemNames[8] = "Apple";
+        itemPrices[8] = 0.22;
+        itemStocks[8] = 10;
+
+        //Options Menu
+        Scanner input = new Scanner(System.in);
+
+        while (true){
+            System.out.println("=== Menu === \n 1. See Inventory \n 2. Restock Item \n 3. Exit ");
+            System.out.print("Select an option: ");
+
+            int select = input.nextInt();
+            input.nextLine();
+
+            switch(select){
+                case 1:
+                    printInventory(itemNames, itemPrices, itemStocks);
+                    break;
+                case 2:
+                    System.out.print("Enter name of item to restock:");
+                    String itemTarget = input.nextLine();
+                    System.out.print("enter amount to add to stock:");
+                    int amount = input.nextInt();
+                    
+                    restockItem(itemNames, itemStocks, itemTarget, amount);
+                    break;
+                case 3: 
+                    //Ends program through 'return'
+                    System.out.println("Closing menu...");
+                    return;
+                default:
+                    System.out.println("invalid choice.");
+                    break;
+
+            }
+
+
+        }
+
     }
   
     public static void printInventory(String[] names, double[] prices, int[] stocks){
